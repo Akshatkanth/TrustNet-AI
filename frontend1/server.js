740d459132fc16c9f976985c29254ea90b663460
Akshat <110756105+Akshatkanth@users.noreply.github.com>
2026-01-08 16:06:15 +0530

    Fix SPA routing - handle page refresh on all routes

--- frontend1/server.js
+++ frontend1/server.js
@@ -7,6 +7,9 @@ const server = http.createServer((req, res) => {
   return handler(req, res, {
     public: 'dist',
     cleanUrls: true,
+    rewrites: [
+      { source: '**', destination: '/index.html' }
+    ],
     headers: [
       {
         source: '**/*',
